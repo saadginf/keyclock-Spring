8c473c0367bdb0329b3d82d2994bf884d0ddad51
Saad Abbou <saadevops@Saads-MacBook-Pro.local>
2020-10-29 22:48:09 +0100

    config done

--- src/main/java/com/testsec/sec/config/KeycloakSpringSecurityConfig.java
+++ src/main/java/com/testsec/sec/config/KeycloakSpringSecurityConfig.java
@@ -1,28 +1,46 @@
 package com.testsec.sec.config;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 
 @KeycloakConfiguration
 public class KeycloakSpringSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
     }
-    
- @Override
- protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.authenticationProvider(keycloakAuthenticationProvider());
-}
-@Override
-protected void configure(HttpSecurity http) throws Exception {
-    super.configure(http);
-    http.authorizeRequests().antMatchers("/books/**").hasAuthority("admin");
-}
+
+    @Bean
+    @Override
+    protected NullAuthenticatedSessionStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests().antMatchers("/books/**").hasAuthority("admin");
+    }
 }
